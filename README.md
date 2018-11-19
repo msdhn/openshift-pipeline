eae7348d55a98f902c855a8a7a757d7ecad6fdcb
Masud Hasan <masud.hasan@ebpi.nl>
2018-11-19 17:23:35 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-./createdeploy.sh
+Requirements:
+- to run locally , minishift should be installed
+- should be logged in to the environment
+    
+ run ./deployservices.sh
+ 
+ on local system browse to : https://192.168.99.100:8443
+
+ 
+ 
--- deployservices.sh
+++ deployservices.sh
@@ -5,7 +5,7 @@ oc new-project dev --description="DEV" --display-name="DEV"
 oc new-project stage --description="STAGE fo" --display-name="STAGE"
 oc new-project cicd --description="CICD" --display-name="CICD"
 
-oc new-app -f ./gogs-openshift/gogs-template.yaml --param
+oc new-app -f ./gogs-openshift/gogs-template.yaml
 oc new-app -f ./nexus-openshift/nexus-template.yaml
 oc new-app -f ./sonarqube-openshift/sonarqube-postgresql-template.yaml --param=SONARQUBE_VERSION=6.7
 oc new-app -f ./jenkins-openshift/jenkins-template.yaml
@@ -17,6 +17,6 @@ oc project cicd
 oc policy add-role-to-group edit system:serviceaccounts:cicd -n dev
 oc policy add-role-to-group edit system:serviceaccounts:cicd -n stage
 
-
+oc project cicd
 oc new-app -f ./jenkins-openshift/jenkins-pipeline-template.yaml -n cicd
 
